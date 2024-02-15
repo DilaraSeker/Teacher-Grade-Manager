9658dcf8af7245057fe6e3703b847da189d53510
DilaraSeker <dlrseker@gmail.com>
2024-02-15 15:29:11 +0300

    Edit H2 properties and edit dependencies

--- src/main/java/com/dilaraseker/teachergrademanager/TeacherGradeManagerApplication.java
+++ src/main/java/com/dilaraseker/teachergrademanager/TeacherGradeManagerApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TeacherGradeManagerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TeacherGradeManagerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TeacherGradeManagerApplication.class, args);
+    }
 
 }
