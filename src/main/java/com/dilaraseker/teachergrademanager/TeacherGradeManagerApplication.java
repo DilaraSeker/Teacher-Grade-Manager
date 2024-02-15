78a0523a0db9cdadaf3224f528e1c5913f311987
DilaraSeker <dlrseker@gmail.com>
2024-02-15 15:00:18 +0300

    Add project files

---
+++ src/main/java/com/dilaraseker/teachergrademanager/TeacherGradeManagerApplication.java
@@ -0,0 +1,13 @@
+package com.dilaraseker.teachergrademanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeacherGradeManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeacherGradeManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dilaraseker/teachergrademanager/TeacherGradeManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dilaraseker.teachergrademanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeacherGradeManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
