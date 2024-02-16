75cfc0f5563dfd9fdfa91553728ed26da160317e
DilaraSeker <dlrseker@gmail.com>
2024-02-17 02:21:26 +0300

    Change method's return type

--- src/main/java/com/dilaraseker/teachergrademanager/repository/GradeRepository.java
+++ src/main/java/com/dilaraseker/teachergrademanager/repository/GradeRepository.java
@@ -3,11 +3,11 @@ package com.dilaraseker.teachergrademanager.repository;
 import com.dilaraseker.teachergrademanager.model.Grade;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import java.util.List;
 
 @Repository
 public interface GradeRepository extends JpaRepository<Grade, Long> {
-    List<Grade> findByStudentId(Long studentId);
-    List<Grade> findByCourseId(Long courseId);
-    List<Grade> findByStudentIdAndCourseId(Long studentId, Long courseId);
+    Grade findByStudentStudentId(Long studentId);
+    Grade findByCourseCourseId(Long courseId);
+    Grade findByStudentStudentIdAndCourseCourseId(Long studentId, Long courseId);
+    Grade findByStudentStudentIdAndCourseExamId(Long studentId, int examID);
 }
