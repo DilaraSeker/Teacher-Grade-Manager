0012f237385d95c1e7d632eca9ce5592e37bf23c
DilaraSeker <dlrseker@gmail.com>
2024-02-17 02:21:43 +0300

    Add findByStudentId method

--- src/main/java/com/dilaraseker/teachergrademanager/repository/StudentRepository.java
+++ src/main/java/com/dilaraseker/teachergrademanager/repository/StudentRepository.java
@@ -7,4 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
     Student findByStudentNumber(String studentNumber);
+
+    Student findByStudentId(Long studentId);
 }
