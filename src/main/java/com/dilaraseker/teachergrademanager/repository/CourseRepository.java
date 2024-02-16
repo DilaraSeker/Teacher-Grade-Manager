6de957b88af49cc03862b4d5a2cc9d92ef0a64d0
DilaraSeker <dlrseker@gmail.com>
2024-02-17 02:16:36 +0300

    Add findByCourseId method

--- src/main/java/com/dilaraseker/teachergrademanager/repository/CourseRepository.java
+++ src/main/java/com/dilaraseker/teachergrademanager/repository/CourseRepository.java
@@ -1,11 +1,13 @@
 package com.dilaraseker.teachergrademanager.repository;
 
 import com.dilaraseker.teachergrademanager.model.Course;
+import com.dilaraseker.teachergrademanager.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface CourseRepository extends JpaRepository<Course, Long> {
-
+    Course findByCourseId(Long id);
 }
