b5028ec4d7fbded056e65e52c786d527d13f2a2c
DilaraSeker <Dilara@DESKTOP-BI2GG8E>
2024-02-17 07:47:55 +0300

    Add Modal for details

--- frontend/src/components/AddGrade.jsx
+++ frontend/src/components/AddGrade.jsx
@@ -8,10 +8,9 @@ const AddGrade = () => {
   const [courseId, setCourseId] = useState('');
   const [examNumber, setExamNumber] = useState('');
   const [grade, setGrade] = useState('');
+  const [message, setMessage] = useState('');
 
-  // Veri alma işlemi
   useEffect(() => {
-    // Öğrenci verilerini al
     axios.get('http://localhost:8080/api/students')
       .then(response => {
         setStudents(response.data);
@@ -19,9 +18,7 @@ const AddGrade = () => {
       .catch(error => {
         console.error('Error fetching students:', error);
       });
-
-    // Ders verilerini al
-    axios.get('http://localhost:8080/courses') // Değişiklik yapıldı: 'http://localhost:8080/courses' yerine 'http://localhost:8080/api/courses'
+    axios.get('http://localhost:8080/api/courses') 
       .then(response => {
         setCourses(response.data);
       })
@@ -29,10 +26,9 @@ const AddGrade = () => {
         console.error('Error fetching courses:', error);
       });
   }, []);
+
   const handleSubmit = async e => {
     e.preventDefault();
-    console.log(studentId);
-    console.log('course id:', courseId);
     try {
       const response = await axios.post('http://localhost:8080/api/grades/save', {
         studentId: parseInt(studentId),
@@ -40,60 +36,82 @@ const AddGrade = () => {
         examNumber: parseInt(examNumber),
         grade: parseFloat(grade)
       });
-      
-      console.log('Response from server:', response); // Sunucudan gelen yanıtı kontrol et
-      console.log('Grade added successfully:', response.data);
+
+      console.log('Response from server:', response); 
+      setMessage('Not Başarıyla Eklendi');
+      // Formu sıfırla
+      setStudentId('');
+      setCourseId('');
+      setExamNumber('');
+      setGrade('');
     } catch (error) {
       console.error('Error adding grade:', error);
       if (error.response) {
         console.error('Server response:', error.response.data);
       }
+      setMessage('Not Eklenmedi! Sorun var!');
     }
   };
-  
-  
 
   return (
     <div className="container">
       <div className="mt-5 form-container">
-        <h3>Not Ekle</h3>
         <form onSubmit={handleSubmit}>
+        <legend>Add Grade</legend>
           <div className="form-group">
-            <label>Öğrenci:</label>
-            <select value={studentId} onChange={e => setStudentId(e.target.value)}>
-              <option value="">Öğrenci Seçin</option>
+            <label>Student:</label>
+            <select className="form-control" value={studentId} onChange={e => setStudentId(e.target.value)}>
+              <option value="">Choose Student</option>
               {students.map(student => (
                 <option key={student.studentId} value={student.studentId}>{student.firstName} {student.lastName}</option>
               ))}
             </select>
           </div>
           <div className="form-group">
-            <label>Ders:</label>
-            <select value={courseId} onChange={e => setCourseId(e.target.value)}>
-              <option value="">Ders Seçin</option>
+            <label>Course:</label>
+            <select className="form-control" value={courseId} onChange={e => setCourseId(e.target.value)}>
+              <option value="">Choose Course</option>
               {courses.map(course => (
                 <option key={course.courseId} value={course.courseId}>{course.courseName}</option>
               ))}
             </select>
           </div>
           <div className="form-group">
-            <label>Sınav Numarası:</label>
+            <label>Exam No:</label>
             <input
               type="number"
+              className="form-control"
               value={examNumber}
               onChange={e => setExamNumber(e.target.value)}
             />
           </div>
           <div className="form-group">
-            <label>Not:</label>
+            <label>Grade:</label>
             <input
               type="number"
+              className="form-control"
               value={grade}
               onChange={e => setGrade(e.target.value)}
             />
           </div>
-          <button type="submit">Ekle</button>
+          <button type="submit" className="btn btn-primary mr-2" style={{ backgroundColor: '#7b6bf0' }}>ADD</button>
         </form>
+        {/* Bootstrap modal */}
+        <div className="modal" tabIndex="-1" role="dialog" style={{ display: message ? 'block' : 'none' }}>
+          <div className="modal-dialog" role="document">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title">Bilgilendirme</h5>
+                <button type="button" className="close" data-dismiss="modal" aria-label="Close" onClick={() => setMessage('')}>
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div className="modal-body">
+                <p>{message}</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
